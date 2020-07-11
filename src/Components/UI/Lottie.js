929cab4332dcb69375eef0c12ecd5700229bd30e
arianna choza <unachoza@gmail.com>
2020-07-11 16:45:00 -0400

    Grid item keep stacking without issues

--- src/Components/UI/Lottie.js
+++ src/Components/UI/Lottie.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import Lottie from 'react-lottie';
 import { makeStyles, useTheme } from '@material-ui/core/styles';
+import useMediaQuery from '@material-ui/core/useMediaQuery';
 import Typography from '@material-ui/core/Typography';
 import { Link } from 'react-router-dom';
 import Grid from '@material-ui/core/Grid';
 import animationData from 'animations/landinganimation/data';
+import customSoftwareIcon from 'assets/Custom Software Icon.svg';
+import mobileAppsIcon from 'assets/mobileIcon.svg';
+import websitesIcon from 'assets/websiteIcon.svg';
+import revolutionBackground from 'assets/repeatingBackground.svg';
+import infoBackground from 'assets/infoBackground.svg';
 import ButtonArrow from 'Components/UI/ButtonArrow';
 import Button from '@material-ui/core/Button';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
 
 const useStyles = makeStyles((theme) => ({
   animation: {
@@ -63,10 +71,32 @@ const useStyles = makeStyles((theme) => ({
       marginLeft: 0,
     },
   },
+  specialText: {
+    fontFamily: 'Pacifico',
+    color: theme.palette.common.orange,
+  },
+  subtitle: {
+    marginBottom: '1em',
+  },
+  icon: {
+    marginLeft: '2em',
+    [theme.breakpoints.down('xs')]: {
+      marginLeft: 0,
+    },
+  },
+  serviceContainer: {
+    marginTop: '12em',
+    [theme.breakpoints.down('sm')]: {
+      padding: 25,
+    },
+  },
 }));
 
 const ThisLottie = () => {
   const classes = useStyles();
+  const theme = useTheme();
+  const matchesSM = useMediaQuery(theme.breakpoints.down('sm'));
+  const matchesXS = useMediaQuery(theme.breakpoints.down('xs'));
   const defaultOptions = {
     loop: true,
     autoplay: false,
@@ -112,11 +142,168 @@ const ThisLottie = () => {
                 </Grid>
               </Grid>
             </Grid>
+            <Grid sm item className={classes.animation}>
+              <Lottie options={defaultOptions} height={'100%'} width={'100%'} />
+            </Grid>
           </Grid>
         </Grid>
-      </Grid>
-      <Grid sm item className={classes.animation}>
-        <Lottie options={defaultOptions} height={'100%'} width={'100%'} />
+        <Grid item>
+          {' '}
+          {/*-----Custom Software Block-----*/}
+          <Grid
+            container
+            direction="row"
+            justify={matchesSM ? 'center' : undefined}
+            className={classes.serviceContainer}
+          >
+            <Grid
+              item
+              style={{
+                marginLeft: matchesSM ? 0 : '5em',
+                textAlign: matchesSM ? 'center' : undefined,
+              }}
+            >
+              <Typography variant="h4">Custom Software Development</Typography>
+              <Typography variant="subtitle1" className={classes.subtitle}>
+                Save Energy. Save Time. Save Money.
+              </Typography>
+              <Typography variant="subtitle1">
+                Complete digital solutions, from investigation to{' '}
+                <span className={classes.specialText}>celebration.</span>
+              </Typography>
+              <Button
+                component={Link}
+                to="/customsoftware"
+                variant="outlined"
+                className={classes.learnButton}
+                // onClick={() => {
+                //   props.setValue(1);
+                //   props.setSelectedIndex(1);
+                // }}
+              >
+                <span style={{ marginRight: 10 }}>Learn More</span>
+                <ButtonArrow width={10} height={10} fill={theme.palette.common.blue} />
+              </Button>
+            </Grid>
+            <Grid item>
+              <img className={classes.icon} alt="custom software icon" src={customSoftwareIcon} />
+            </Grid>
+          </Grid>
+        </Grid>
+        <Grid item>
+          {' '}
+          {/*-----iOS/Android Block-----*/}
+          <Grid
+            container
+            direction="row"
+            justify={matchesSM ? 'center' : 'flex-end'}
+            className={classes.serviceContainer}
+          >
+            <Grid
+              item
+              style={{
+                textAlign: matchesSM ? 'center' : undefined,
+              }}
+            >
+              <Typography variant="h4">iOS/Android App Development</Typography>
+              <Typography variant="subtitle1" className={classes.subtitle}>
+                Extend Functionality. Extend Access. Increase Engagement.
+              </Typography>
+              <Typography variant="subtitle1">
+                Integrate your web experience or create a standalone app
+                {matchesSM ? null : <br />}with either mobile platform.
+              </Typography>
+              <Button
+                component={Link}
+                to="/mobileapps"
+                variant="outlined"
+                className={classes.learnButton}
+                // onClick={() => {
+                //   props.setValue(1);
+                //   props.setSelectedIndex(2);
+                // }}
+              >
+                <span style={{ marginRight: 10 }}>Learn More</span>
+                <ButtonArrow width={10} height={10} fill={theme.palette.common.blue} />
+              </Button>
+            </Grid>
+            <Grid item style={{ marginRight: matchesSM ? 0 : '5em' }}>
+              <img className={classes.icon} alt="mobile phone icon" src={mobileAppsIcon} />
+            </Grid>
+          </Grid>
+        </Grid>
+        <Grid item>
+          {' '}
+          {/*-----Websites Block-----*/}
+          <Grid
+            container
+            direction="row"
+            justify={matchesSM ? 'center' : undefined}
+            className={classes.serviceContainer}
+          >
+            <Grid
+              item
+              style={{
+                marginLeft: matchesSM ? 0 : '5em',
+                textAlign: matchesSM ? 'center' : undefined,
+              }}
+            >
+              <Typography variant="h4">Website Development</Typography>
+              <Typography variant="subtitle1" className={classes.subtitle}>
+                Reach More. Discover More. Sell More.
+              </Typography>
+              <Typography variant="subtitle1">Optimized for Search Engines, built for speed.</Typography>
+              <Button
+                component={Link}
+                to="/websites"
+                variant="outlined"
+                className={classes.learnButton}
+                // onClick={() => {
+                //   props.setValue(1);
+                //   props.setSelectedIndex(3);
+                // }}
+              >
+                <span style={{ marginRight: 10 }}>Learn More</span>
+                <ButtonArrow width={10} height={10} fill={theme.palette.common.blue} />
+              </Button>
+            </Grid>
+            <Grid item>
+              <img className={classes.icon} alt="website icon" src={websitesIcon} />
+            </Grid>
+          </Grid>
+        </Grid>
+        <Grid item>
+          {/*-----The Revolution Block-----*/}
+          <Grid container style={{ height: '100em', marginTop: '12em' }} alignItems="center" justify="center">
+            <Card className={classes.revolutionCard}>
+              <CardContent>
+                <Grid container direction="column" style={{ textAlign: 'center' }}>
+                  <Grid item>
+                    <Typography variant="h3" gutterBottom>
+                      The Revolution
+                    </Typography>
+                  </Grid>
+                  <Grid item>
+                    <Typography variant="subtitle1">
+                      Visionary insights coupled with cutting-edge technology is a recipe for revolution.
+                    </Typography>
+                    <Button
+                      component={Link}
+                      to="/revolution"
+                      className={classes.learnButtonHero}
+                      variant="outlined"
+                      // onClick={() => props.setValue(2)}
+                    >
+                      <span style={{ marginRight: 10 }}>Learn More</span>
+                      <ButtonArrow width={15} height={15} fill={theme.palette.common.blue} />
+                    </Button>
+                  </Grid>
+                </Grid>
+              </CardContent>
+            </Card>
+            <div className={classes.revolutionBackground} />
+          </Grid>
+        </Grid>
       </Grid>
     </>
   );
