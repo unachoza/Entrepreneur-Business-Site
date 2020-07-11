39abceb8e866c54d0a7d2c84e65273a8b450d044
arianna choza <unachoza@gmail.com>
2020-07-11 16:05:55 -0400

    got a component to render in another component

---
+++ src/Components/About.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Grid from '@material-ui/core/Grid';
+// import USER from 'Constants/constant';
+
+const Contact = () => {
+  return (
+    <Grid>
+      <Grid item>
+        <div className="title">Contact us</div>
+      </Grid>
+      <div>
+        <div className="title">Contact us</div>
+      </div>
+    </Grid>
+  );
+};
+export default Contact;
--- src/Components/UI/Lottie.js
+++ src/Components/UI/Lottie.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import Lottie from 'react-lottie';
 import { makeStyles, useTheme } from '@material-ui/core/styles';
-
+import Homepage from 'Components/H';
+import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid';
 import animationData from 'animations/landinganimation/data';
+import ButtonArrow from 'Components/UI/ButtonArrow';
+import Button from '@material-ui/core/Button';
 
 const ThisLottie = () => {
   const defaultOptions = {
@@ -13,6 +17,32 @@ const ThisLottie = () => {
       preserveAspectRatio: 'xMidYMid slice',
     },
   };
-  return <Lottie options={defaultOptions} height={'100%'} width={'100%'} />;
+  return (
+    <>
+      <Grid container direction="column">
+        <Grid item>
+          <Grid container direction="row">
+            <Grid item>
+              <Typography variant="h2" align="center">
+                Bringing West Coast Technology
+                <br />
+                to the Midwest
+              </Typography>
+              <Grid item>
+                <Button variant="contained">Free Estimate</Button>
+                <Grid item>
+                  <Button variant="outlined">
+                    Learn More
+                    <ButtonArrow width={15} height={15} fill="red" />
+                  </Button>
+                </Grid>
+              </Grid>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
+      <Lottie options={defaultOptions} height={'100%'} width={'100%'} />
+    </>
+  );
 };
 export default ThisLottie;
